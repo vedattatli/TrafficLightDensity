8d1062a60ce54f602974ab9779c5c4ac153b409b
vedattatli <1030510305@erciyes.edu.tr>
2025-05-25 20:43:06 +0300

    Update LightPhase.java

--- src/main/java/com/erciyes/edu/tr/trafficlightdensity/road_objects/LightPhase.java
+++ src/main/java/com/erciyes/edu/tr/trafficlightdensity/road_objects/LightPhase.java
@@ -1,4 +1,5 @@
 package com.erciyes.edu.tr.trafficlightdensity.road_objects;
+import com.erciyes.edu.tr.trafficlightdensity.brain.TrafficController;
 import com.erciyes.edu.tr.trafficlightdensity.intersection_gui.UserInterfaceController;
 import javafx.scene.paint.Color;
 
@@ -18,7 +19,7 @@ public enum LightPhase {
 
     public static Duration getDefaultPhaseDuration(LightPhase phase) {
         return switch (phase) {
-            case YELLOW -> Duration.ofSeconds(3); // sabit
+            case YELLOW -> Duration.ofSeconds(TrafficController.YELLOW_DURATION); // sabit
             case RED, GREEN -> Duration.ofSeconds(0); // dışarıdan atanacak
         };
     }
